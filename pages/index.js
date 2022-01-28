c0b4cb877b86e6601a34514a296ee2273ea432cb
Enzo Noda <enzo.noda@gmail.com>
2022-01-27 23:45:39 -0300

    primeira modificação

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import { route } from 'next/dist/server/router'
 
 function Titulo(props) {
   const Tag = props.tag || 'h1'
@@ -48,25 +20,12 @@ function Titulo(props) {
   )
 }
 
-// Componente React
-// function HomePage() {
-//     // JSX
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-// export default HomePage
-
 export default function PaginaInicial() {
-  const username = 'peas'
+  const [username, setUsername] = useState('EnzoNoda')
+  const router = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex',
@@ -101,6 +60,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={info => {
+              info.preventDefault()
+              router.push('/chat')
+            }}
             styleSheet={{
               display: 'flex',
               flexDirection: 'column',
@@ -123,6 +86,9 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              onChange={event => {
+                setUsername(event.target.value)
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
